bad931eb9f58a79c60b7d40a741ed8fb4d68d2a1
Dakota Soares <78927290+DakotaCS@users.noreply.github.com>
2024-10-22 14:42:08 -0400

    Update client.js

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
     "https://vudfoksgebxejqxjgnli.supabase.co", 
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1ZGZva3NnZWJ4ZWpxeGpnbmxpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk5MjUxMzEsImV4cCI6MjAxNTUwMTEzMX0.bjRh4E9fsYZTtF5l58PChhYA4IXuZDq8xdB3qeZYlks"
+    "#####################"
 )
